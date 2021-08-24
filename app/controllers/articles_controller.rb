718a6ca0492ed026de020aa8bb9636d4537f2fd7
yugotokonami <18b1123@andrew.ac.jp>
2021-08-24 09:48:15 +0900

    記事投稿機能の編集（最低限の実装）

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,22 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:image, :title, :text, :category_id, ).merge(user_id: current_user.id)
+  end
+
 end
