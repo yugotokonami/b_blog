a4cb2d34f46f93dd7fa889afaaf58a06336eb9ec
yugotokonami <18b1123@andrew.ac.jp>
2021-08-31 11:26:59 +0900

    URL遷移防止機能の追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_article, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :new, :create, :show,]
 
   def index
     @articles = Article.order("created_at DESC")
@@ -56,4 +58,9 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def move_to_index
+    unless current_user.id == @article.user.id
+      redirect_to action: :index
+    end
+  end
 end
